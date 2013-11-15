81af7fe78d86f8c4230c42c301529fd80d27756f
sealocal <local.mat@gmail.com>
2013-11-15 13:21:57 -0800

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship", #necessary because otherwise Rails would look for class ReverseRelationships
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships #, source: :follower
+  default_scope -> { order('created_at DESC') }
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
@@ -19,8 +26,21 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
+  end
+
+  def following?(other_user)
+    #equivalent to line below: relationships.find_by(followed_id: other_user.id)
+    self.relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    #equivalent to line below: relationships.create!(followed_id: other_user.id)
+    self.relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    self.relationships.find_by(followed_id: other_user.id).destroy!
   end
 
   private
